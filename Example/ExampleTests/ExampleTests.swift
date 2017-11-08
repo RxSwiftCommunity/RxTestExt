b285b929cefd319d517054dd305387e679beccfb
mosamer <mostafamamer@gmail.com>
2017-11-08 15:48:14 +0100

    Introduce record & bind to `TestScheduler`
    Extend `TestScheduler` with utility functions to;
    - record an observable emitted events on a testable observer
    - bind array of recorded events to an observer

--- Example/ExampleTests/ExampleTests.swift
+++ Example/ExampleTests/ExampleTests.swift
@@ -1,13 +1,27 @@
 import XCTest
+import RxTest
 import RxTestExt
+@testable import Example
 
 class ExampleTests: XCTestCase {
-    
+
+    var viewModel: ViewModel!
+    var scheduler: TestScheduler!
+
     override func setUp() {
         super.setUp()
-
+        viewModel = ViewModel()
+        scheduler = TestScheduler(initialClock: 0)
     }
-    
+
+    func testRecordingAllEvents() {
+        let events = [next(10, "alpha"), completed(10)]
+        let source = scheduler.record(source: viewModel.elements)
+        scheduler.bind(events, to: viewModel.input)
+        scheduler.start()
+        XCTAssertEqual(source.events, events)
+    }
+
     override func tearDown() {
         super.tearDown()
     }
---
+++ RxTestExt/Core/TestScheduler+Ext.swift
@@ -0,0 +1,36 @@
+import RxSwift
+import RxTest
+
+extension TestScheduler {
+
+    /// Builds testable observer for s specific observable sequence, binds it's results and sets up disposal.
+    ///
+    //  This function was copied from RxSwift's example app:
+    //  https://github.com/ReactiveX/RxSwift/blob/f639ff450487340a18931a7dbe3d5c8a0976be7b/RxExample/RxExample-iOSTests/TestScheduler%2BMarbleTests.swift#L189
+    //
+    /// - Parameter source: Observable sequence to observe.
+    /// - Returns: Observer that records all events for observable sequence.
+    public func record<O: ObservableConvertibleType>(source: O) -> TestableObserver<O.E> {
+        let observer = self.createObserver(O.E.self)
+        let disposable = source.asObservable().subscribe(observer)
+        self.scheduleAt(100000) {
+            disposable.dispose()
+        }
+        return observer
+    }
+
+    /// Binds given events to an observer
+    ///
+    /// Builds a hot observable with predefined events, binds it's result to given observer and sets up disposal.
+    ///
+    /// - Parameters:
+    ///   - events: Array of recorded events to emit over the scheduled observable
+    ///   - target: Observer to bind to
+    public func bind<O: ObserverType>(_ events: [Recorded<Event<O.E>>], to target: O) {
+        let driver = self.createHotObservable(events)
+        let disposable = driver.asObservable().subscribe(target)
+        self.scheduleAt(100000) {
+            disposable.dispose()
+        }
+    }
+}
