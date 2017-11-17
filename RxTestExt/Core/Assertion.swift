ad16ae8780da16e8a242307fcf877ebe1c9c13ee
mosamer <mostafamamer@gmail.com>
2017-11-17 17:47:52 +0100

    Introduce `Assertion`

--- RxTestExt/Core/Assertion.swift
+++ RxTestExt/Core/Assertion.swift
@@ -12,3 +12,11 @@ func haveError<T>(_ events: [Recorded<Event<T>>]) -> Bool {
 func haveCompleted<T>(_ events: [Recorded<Event<T>>]) -> Bool {
     return events.last?.value.isCompleted ?? false
 }
+
+public struct Assertion<T> {
+
+}
+
+public func assert<T>(_ source: TestableObserver<T>, file: StaticString = #file, line: UInt = #line) -> Assertion<T> {
+    return Assertion()
+}
