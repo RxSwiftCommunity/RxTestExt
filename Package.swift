2348322aa33d4969b540586d77a16c0e8125ed3e
mosamer <mostafamamer@gmail.com>
2020-08-14 08:42:12 +0200

    Cleanup Package file

--- Package.swift
+++ Package.swift
@@ -6,7 +6,6 @@ import PackageDescription
 let package = Package(
     name: "RxTestExt",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "RxTestExt",
             targets: ["RxTestExt"]),
